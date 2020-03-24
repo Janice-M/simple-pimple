6a1859adb4804fa32deedfa5e730da3da9d6f045
Janice Muia <janiceink001@gmail.com>
2020-03-24 23:09:12 +0300

    class mycomponent

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,2 +1,14 @@
 import React, {Component} from 'react';
 
+class MyComponent extends Component{
+    render(){
+
+        return (
+            <div> HELLO SHEA BUTTER BABY </div>
+        )
+    }
+
+
+};
+
+export default MyComponent;
